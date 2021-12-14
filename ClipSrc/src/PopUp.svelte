cd83d2472b8a18e710709c620998ad3e05e0dc07
Lauren Hale <lauren.m.hale7@gmail.com>
2021-12-14 13:32:20 -0600

    adds text and url to extension popup on changes

--- ClipSrc/src/PopUp.svelte
+++ ClipSrc/src/PopUp.svelte
@@ -1,12 +1,17 @@
 
 <script>
     let textSnippet;
+    let url;
        chrome.storage.local.get(['txtS'], function(result) {
         console.log('Value currently is ' + result.txtS);
         textSnippet = result.txtS
     });
 
-
+    chrome.storage.local.get(["url"],
+    function(result) {
+        console.log("Url is" + result.url)
+        url = result.url
+    });
     function handleClick() {
     
         alert(textSnippet)
@@ -17,6 +22,7 @@
 <main>
     <h1>ClipSRC Extension</h1>
     <form>
+        <input value={url}> 
         <textarea bind:value={textSnippet}/>
         <button id="save_button" on:click={handleClick}>Save</button>
     </form>
