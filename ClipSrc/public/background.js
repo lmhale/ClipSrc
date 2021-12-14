c3054b72b691ea3721c669f900ac7f7f4b094e4b
Lauren Hale <lauren.m.hale7@gmail.com>
2021-12-13 23:02:36 -0600

    popup.js can get data from highlighed text set in storage

--- ClipSrc/public/background.js
+++ ClipSrc/public/background.js
@@ -3,5 +3,10 @@ console.log('background running')
 chrome.runtime.onMessage.addListener(receiver)
 
 function receiver(request, sender, response){
-    console.log(request)
+    let textSnippet = request.text
+    console.log(textSnippet)
+  chrome.storage.local.set({txtS: textSnippet}, function() {
+  console.log('Value is set to ' + textSnippet);
+});
+
 }
