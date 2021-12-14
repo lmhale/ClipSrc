77aac9b12502447bd314d67550cac3d127d0ad51
Lauren Hale <lauren.m.hale7@gmail.com>
2021-12-14 14:55:30 -0600

    style(ext) inject css in manifest and add tag select in svelte popup

--- ClipSrc/public/content.js
+++ ClipSrc/public/content.js
@@ -2,8 +2,13 @@
 
 window.addEventListener("mouseup", selectWords)
 
+
+
   function selectWords(){
+   
+
     let textSnippet = window.getSelection().toString()
+    console.log('selected:', window.getSelection())
     console.log(textSnippet)
     if(textSnippet.length > 0){
       let message = {
--- ClipSrc/src/PopUp.svelte
+++ ClipSrc/src/PopUp.svelte
@@ -1,5 +1,15 @@
 
 <script>
+//will dynamically grab tags from back end
+let tags = [
+		{ id: 1, text: `quickCapture` },
+		{ id: 2, text: `history` },
+		{ id: 3, text: `coding` }
+	];
+
+    let selected;
+    let answer;
+
     let textSnippet;
     let url;
        chrome.storage.local.get(['txtS'], function(result) {
@@ -12,19 +22,28 @@
         console.log("Url is" + result.url)
         url = result.url
     });
-    function handleClick() {
-    
-        alert(textSnippet)
-     
+  
+    function handleSubmit() {
+        let info = textSnippet + '\n' + selected.text
+        alert(info)
     }
 </script>
 
 <main>
     <h1>ClipSRC Extension</h1>
-    <form>
-        <input value={url}> 
+    <form on:submit|preventDefault={handleSubmit}>
+        <label for="link">Link:</label>
+        <input name="link" value={url}> 
         <textarea bind:value={textSnippet}/>
-        <button id="save_button" on:click={handleClick}>Save</button>
+        <label for="tag">Tag:</label>
+        <select for="tag" bind:value={selected} on:change="{() => selected = selected}" >
+            {#each tags as tag}
+                <option value={tag}>
+                    {tag.text}
+                </option>
+            {/each}
+        </select>
+        <button id="save_button" type=submit>Save</button>
     </form>
 </main>
 
