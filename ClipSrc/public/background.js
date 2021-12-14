331520114505fac549b49c96e7b222b8b9bf914d
Lauren Hale <lauren.m.hale7@gmail.com>
2021-12-13 20:55:01 -0600

    can get text on select and url

--- ClipSrc/public/background.js
+++ ClipSrc/public/background.js
@@ -1,7 +1,7 @@
 
-let color = '#3aa757';
+console.log('background running')
+chrome.runtime.onMessage.addListener(receiver)
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
+function receiver(request, sender, response){
+    console.log(request)
+}
--- ClipSrc/public/content.js
+++ ClipSrc/public/content.js
@@ -1,24 +1,45 @@
-let changeColor = document.getElementById("save");
-
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+//  let saveButton= document.getElementById("save");
+// saveButton.addEventListener("click",async ()=> {
+//   let url = window.location.href
+//   console.log("hi", url)
+// })
+// chrome.storage.sync.get("color", ({ color }) => {
+//   changeColor.style.backgroundColor = color;
+// });
 
 
 // When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
-});
+// changeColor.addEventListener("click", async () => {
+//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+//   console.log("tab", tab)
+//   chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     function: getCurrentUrl,
+//   });
+// });
 
 // The body of this function will be executed as a content script inside the
 // current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
-}
+// function setPageBackgroundColor() {
+//   chrome.storage.sync.get("color", ({ color }) => {
+//     document.body.style.backgroundColor = color;
+//   });
+// }
+
+window.addEventListener("mouseup", selectWords)
+
+  function getCurrentUrl(){
+    // let url = window.location.href
+    // console.log("hi", url)
+  }
+  function selectWords(){
+    let textSnippet = window.getSelection().toString()
+    console.log(textSnippet)
+    if(textSnippet.length > 0){
+      let message = {
+        text:textSnippet,
+        url: window.location.href
+      }
+      chrome.runtime.sendMessage(message)
+    }
+  }
